edeeaa80340c0f14cdaf3c11aefa850547fbb1e5
David Martinez <david.mtz00@hotmail.com>
2019-06-20 15:42:22 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kwic/precache-manifest.4591febb5a9a388a1a7cea4a270cca6d.js"
+  "/kwic/precache-manifest.8e23b07d3c44565dc6679dfc7133f71e.js"
 );
 
 self.addEventListener('message', (event) => {
